040729e86a5f4a2fb2619099356b85f66a4544aa
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-29 14:14:09 +0800

    Add Threading to main.py for receive/send functionality

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from server import server
 import time
+from threading import Thread
+
+def receive_message(input_server):
+    while(True):
+        incomingMessage = input_server.read()
+        if(incomingMessage != None):
+            print("Message from client: "+incomingMessage);
+        time.sleep(2)
+
+def send_message(input_server, message):
+    input_server.write(message)
+    print("Message to client: "+message)
 
 test_server = server()
 test_server.connect()
-test_server.write("Hello from Server!".encode()) # Send a test message to client
-while(True):
-    incomingMessage = test_server.read()
-    if(incomingMessage != None):
-        print(incomingMessage)
-    time.sleep(2000)
+receive_thread = Thread(target=receive_message, args=(test_server,))
+send_thread = Thread(target=send_message, args=(test_server, "Hello from Server!"))
+receive_thread.start()
+send_thread.start()
